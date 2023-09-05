b08b3e8c6e51420b20e4e1ce0319272b7f001448
chovrio <1480733667@qq.com>
2023-09-05 13:13:09 +0800

    refactor: none

--- playground/demo-vue/chovrio.config.js
+++ playground/demo-vue/chovrio.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'chovrio';
-import vue from '@vitejs/plugin-vue';
+// import { defineConfig } from 'chovrio';
+// import vue from '@vitejs/plugin-vue';
+import { vueParsePlugin } from 'vue-parse-plugin';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()]
-});
+export default {
+  plugins: [vueParsePlugin()]
+};
--- playground/demo-vue/src/main.ts
+++ playground/demo-vue/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue';
 import './style.css';
+import svg from './assets/vue.svg';
 import App from './App.vue';
-
+console.log(svg);
 createApp(App).mount('#app');
